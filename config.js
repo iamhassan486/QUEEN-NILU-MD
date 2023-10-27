f66f19e6120e8c4fe5a934ad46344d92c3ed7ca9
iamhassan486 <148365676+iamhassan486@users.noreply.github.com>
2023-10-27 14:25:58 +0500

    Update config.js

--- config.js
+++ config.js
@@ -17,17 +17,17 @@ function convertToBool(text, fault = 'true') {
 global.SESSION_ID = '' // PUT your session ID BY scan QR 
 
 // databse URL  ##Required 
-global.POSTGRESQL_URL = ''
+global.POSTGRESQL_URL = 'postgres://nilu_md_t2w8_user:jbU5CmbGsunE2ENOeuN4vEJc7zEUrGLF@dpg-cktnbb6nfb1c73fml1qg-a.oregon-postgres.render.com/nilu_md_t2w8'
 
 
 
 // ⚙️ OWNER SETTINGS  ⚙️
 
-global.OWNER_NUMBER = '94767438882'
+global.OWNER_NUMBER = '923086657240'
 
-global.OWNER_NAME = 'Janiya_X'
+global.OWNER_NAME = 'Hassan'
 
-global.TIMEZONE = 'Asia/Colombo' 
+global.TIMEZONE = 'Asia/Karachi' 
 
 global.INBOX_BLOCK_MESSAGE = '' // Inboc Block Message
 
@@ -76,7 +76,7 @@ global.GROUP_LINK = 'https://chat.whatsapp.com/IRdyJY5P4sF5BhtP5PIdHH' // ur gro
 
 /* 18 + Download Settings */
 
-global.SEX_DL = 'false' // 18 + video dl
+global.SEX_DL = 'true' // 18 + video dl
 
 /* AUTO REPLY SETTINGS */
 
